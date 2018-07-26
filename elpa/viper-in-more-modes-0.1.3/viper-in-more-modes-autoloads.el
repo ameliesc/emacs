18d6d73a194e879ffa82ddefeb63c6be5a85e9de
Amelie Schmidt-Colberg <amelie.schmidtcolberg@gmail.com>
2018-07-26 20:39:12 +0900

    emacs settings, backupf or reinstall

---
+++ elpa/viper-in-more-modes-0.1.3/viper-in-more-modes-autoloads.el
@@ -0,0 +1,16 @@
+;;; viper-in-more-modes-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("viper-in-more-modes.el") (22987 21364
+;;;;;;  0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; viper-in-more-modes-autoloads.el ends here
---
+++ elpa/viper-in-more-modes-0.1.3/viper-in-more-modes-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "viper-in-more-modes" "0.1.3" "vi-like keybindings for various Emacs modes" 'nil)
